519b2852ca949a3d059c2022f0dd745ecd5c98fb
ashpil <shpilenok2001@gmail.com>
2021-04-13 20:00:21 -0400

    more documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
 #![feature(proc_macro_span)]
 #![feature(proc_macro_diagnostic)]
 
+//! A tiny proc macro to include a WGSL file in your binary, and verify that it is valid at compile time.
+
 use proc_macro::{TokenStream, Span, Diagnostic, Level};
 use syn::{parse_macro_input, LitStr};
 use naga::{valid::{ValidationFlags, Validator}, front::wgsl};
 
-/// Works just like the standard library's `include_str!` macro, but errors on compile time if the
-/// contents are not valid WGSL.
+/// Just like `include_str!`, but errors on compile time if the contents are not valid WGSL.
+///
+/// # Example
+///
+/// ```
+/// let shader_str = include_wgsl!("shader.wgsl");
+/// device.create_shader_module(&ShaderModuleDescriptor {
+///     source: ShaderSource::Wgsl(Cow::Borrowed(&shader_str)),
+///     flags: ShaderFlags::default(),
+///     label: None,
+/// })
+/// ```
 #[proc_macro]
 pub fn include_wgsl(input: TokenStream) -> TokenStream {
     let file_path = parse_macro_input!(input as LitStr).value();
