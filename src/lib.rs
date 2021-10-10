ecb02e93b5427caf75f426e4c3c4a36eae6d57a0
ashpil <shpilenok2001@gmail.com>
2021-10-09 20:47:09 -0400

    adds macro that also translates to spv

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,12 @@
 
 use proc_macro::{TokenStream, Span, Diagnostic, Level};
 use syn::{parse_macro_input, LitStr};
-use naga::{valid::{ValidationFlags, Validator}, front::wgsl};
+use naga::{
+    valid::{ValidationFlags, Validator, Capabilities},
+    front::wgsl,
+};
+#[cfg(feature = "spv-out")]
+use::naga::back::spv::{self, Options};
 
 /// Just like `include_str!`, but errors on compile time if the contents are not valid WGSL.
 ///
@@ -37,14 +42,71 @@ pub fn include_wgsl(input: TokenStream) -> TokenStream {
             match wgsl::parse_str(&wgsl_str) {
                 Ok(module) => {
                     // Attempt to validate WGSL, error if invalid
-                    match Validator::new(ValidationFlags::all()).validate(&module) {
+                    match Validator::new(ValidationFlags::all(), Capabilities::all()).validate(&module) {
                         Ok(_) => {},
                         Err(e) => Diagnostic::new(Level::Error, format!("{}: {}", file_path, e)).emit(),
                     }
                 },
                 Err(e) => {
                     Diagnostic::new(Level::Error, format!("Unable to parse {}:", file_path)).emit();
-                    e.emit_to_stderr();
+                    e.emit_to_stderr(&wgsl_str);
+                },
+            }
+        },
+        Err(e) => {
+            Diagnostic::spanned(call_site, Level::Error, format!("couldn't read {}: {}", file_path, e));
+        },
+    };
+
+    // just return the `include_str!` macro with the given file path
+    format!("&include_str!(\"{}\")", file_path).parse().unwrap()
+}
+
+/// Just like `include_str!`, but errors on compile time if the contents are not valid WGSL. 
+/// If the contents are valid WGSL, this returns the corresponding SPIR-V bytecode as a u32 array.
+///
+/// # Example
+///
+/// ```
+/// let shader_str = include_wgsl_to_spv!("shader.wgsl");
+/// device.create_shader_module(&ShaderModuleDescriptor {
+///     source: ShaderSource::Wgsl(Cow::Borrowed(&shader_str)),
+///     flags: ShaderFlags::default(),
+///     label: None,
+/// })
+/// ```
+#[cfg(feature = "spv-out")]
+#[proc_macro]
+pub fn include_wgsl_to_spv(input: TokenStream) -> TokenStream {
+    let file_path = parse_macro_input!(input as LitStr).value();
+    let call_site = Span::call_site();
+    let mut own_path = call_site.source_file().path();
+    // Assert we actually have a valid call site
+    assert!(own_path.pop());
+
+    // This is the path relative to the current working directory
+    let new_path = own_path.join(&file_path);
+
+    // Load string contents, error if path not found
+    match std::fs::read_to_string(new_path) {
+        Ok(wgsl_str) => {
+            // Attempt to parse WGSL, error if invalid
+            match wgsl::parse_str(&wgsl_str) {
+                Ok(module) => {
+                    // Attempt to validate WGSL, error if invalid
+                    match Validator::new(ValidationFlags::all(), Capabilities::all()).validate(&module) {
+                        Ok(info) => {
+                            let spv_bytes = spv::write_vec(&module, &info, &Options::default(), None).expect("Could not generate SPV array");
+                            return format!("{{ include_str!(\"{}\"); {:?} }}", file_path, spv_bytes).parse().expect("Cannot format return SPV array");
+                        },
+                        Err(e) => {
+                            Diagnostic::new(Level::Error, format!("{}: {}", file_path, e)).emit()
+                        },
+                    }
+                },
+                Err(e) => {
+                    Diagnostic::new(Level::Error, format!("Unable to parse {}:", file_path)).emit();
+                    e.emit_to_stderr(&wgsl_str);
                 },
             }
         },
